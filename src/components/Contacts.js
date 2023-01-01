b6f23fa29744e22486fe26c452a10e175f0256eb
Anusree V A <anusreeva93@gmail.com>
2023-01-01 19:52:27 +0530

    Added Contacts Section

---
+++ src/components/Contacts.js
@@ -0,0 +1,9 @@
+import "./contacts.css";
+
+export default function Contacts() {
+  return (
+    <div className="contacts-main-container" id="contact-section">
+      <div className="contacts-main-heading">Contact Me</div>
+    </div>
+  );
+}
