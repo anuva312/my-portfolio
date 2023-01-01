0c47e3df05017148af446b27a28dd7ce0f2962de
Anusree V A <anusreeva93@gmail.com>
2023-01-01 20:26:59 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 // import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   const onSectionClicked = function (sectionName) {
@@ -22,6 +23,7 @@ function App() {
       <Skills />
       <Projects />
       {/* <Contacts /> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import "./footer.css";
+
+export default function Footer() {
+  return <div className="footer-container">Made with ❤ and React by anuVA</div>;
+}
