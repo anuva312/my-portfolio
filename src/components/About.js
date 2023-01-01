69f7eb5f029f65521ead40cc53b94438fefe400f
Anusree V A <anusreeva93@gmail.com>
2023-01-01 20:26:51 +0530

    Added social media links

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,27 @@ import "./About.css";
 import "./animation.css";
 
 export default function About() {
+  const onClickIcon = function (e, type) {
+    e.preventDefault();
+    let url = "";
+    switch (type) {
+      case "github":
+        url = "https://github.com/anuva312";
+        break;
+      case "twitter":
+        url = "https://twitter.com/this_is_anuVA";
+        break;
+      case "linkedin":
+        url = "https://www.linkedin.com/in/anusree-v-a-408554191/";
+        break;
+      default:
+        url = null;
+        break;
+    }
+    if (url) {
+      window.open(url, "_blank");
+    }
+  };
   return (
     <div className="about-main-container" id="about-section">
       <div className="about-main-heading animate slide">
@@ -13,6 +34,23 @@ export default function About() {
         I'm a front end developer. I love making new stuff, playing with code
         and figuring out how stuff works. I aspire to build awe inspiring stuff,
         learn new technologies and keep challenging myself everyday!
+      </div>
+      <div className="about-links-container animate pop delay-4">
+        Come say hii on :{" "}
+        <div className="about-links-icon-container">
+          <i
+            class="fa-brands fa-github about-link-icon"
+            onClick={(e) => onClickIcon(e, "github")}
+          ></i>
+          <i
+            class="fa-brands fa-twitter about-link-icon"
+            onClick={(e) => onClickIcon(e, "twitter")}
+          ></i>
+          <i
+            class="fa-brands fa-linkedin about-link-icon"
+            onClick={(e) => onClickIcon(e, "linkedin")}
+          ></i>
+        </div>
       </div>
     </div>
   );
