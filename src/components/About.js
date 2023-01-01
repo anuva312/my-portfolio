8c70df8f838da198d4097781fc219b676b1bda0b
Anusree V A <anusreeva93@gmail.com>
2023-01-01 19:23:50 +0530

    Style changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import "./About.css";
+import "./animation.css";
 
 export default function About() {
   return (
     <div className="about-main-container" id="about-section">
-      <div className="about-main-heading">Hello there👋, I'm Anusree</div>
-      <div className="about-photo"></div>
-      <div className="about-description">
+      <div className="about-main-heading animate slide">
+        Hello there👋, I'm Anusree
+      </div>
+      <div className="about-photo animate delay-1"></div>
+      <div className="about-description animate pop delay-2">
         {" "}
         I'm a front end developer. I love making new stuff, playing with code
         and figuring out how stuff works. I aspire to build awe inspiring stuff,
