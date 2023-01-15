d9799b8bfb82001e9b8f19eff10b8b85f1e25961
Anusree V A <anusreeva93@gmail.com>
2023-01-15 18:37:50 +0530

    Added tooltip to skill icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,12 +6,12 @@ export default function Skills() {
     <div className="skills-main-container" id="skills-section">
       <div className="skills-main-heading animate">Languages and Tools</div>
       <div className="skills-container animate pop delay-5">
-        <i className="fa-brands fa-react skill-icon"></i>
-        <i className="fa-brands fa-square-js skill-icon"></i>
-        <i className="fa-brands fa-html5 skill-icon"></i>
-        <i className="fa-brands fa-css3-alt skill-icon"></i>
-        <i className="fa-brands fa-python skill-icon"></i>
-        <i className="fa-brands fa-node-js skill-icon"></i>
+        <i className="fa-brands fa-react skill-icon" title="React JS"></i>
+        <i className="fa-brands fa-square-js skill-icon" title="JavaScript"></i>
+        <i className="fa-brands fa-html5 skill-icon" title="HTML"></i>
+        <i className="fa-brands fa-css3-alt skill-icon" title="CSS"></i>
+        <i className="fa-brands fa-python skill-icon" title="Python"></i>
+        <i className="fa-brands fa-node-js skill-icon" title="Node JS"></i>
       </div>
     </div>
   );
