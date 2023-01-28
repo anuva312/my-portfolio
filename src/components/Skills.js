8da8ee09fb556251accb6a40875d19b8527567c3
Anusree V A <anusreeva93@gmail.com>
2023-01-28 20:10:40 +0530

    Added links to skills icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,42 @@
 import "./skills.css";
 import "./animation.css";
+import onClickIcon from "./onClickIcon";
 
 export default function Skills() {
   return (
     <div className="skills-main-container" id="skills-section">
       <div className="skills-main-heading animate">Languages and Tools</div>
       <div className="skills-container animate pop delay-5">
-        <i className="fa-brands fa-react skill-icon" title="React JS"></i>
-        <i className="fa-brands fa-square-js skill-icon" title="JavaScript"></i>
-        <i className="fa-brands fa-html5 skill-icon" title="HTML"></i>
-        <i className="fa-brands fa-css3-alt skill-icon" title="CSS"></i>
-        <i className="fa-brands fa-python skill-icon" title="Python"></i>
-        <i className="fa-brands fa-node-js skill-icon" title="Node JS"></i>
+        <i
+          className="fa-brands fa-react skill-icon"
+          onClick={(e) => onClickIcon(e, "reactjs")}
+          title="React JS"
+        ></i>
+        <i
+          className="fa-brands fa-square-js skill-icon"
+          onClick={(e) => onClickIcon(e, "javascript")}
+          title="JavaScript"
+        ></i>
+        <i
+          className="fa-brands fa-html5 skill-icon"
+          onClick={(e) => onClickIcon(e, "html")}
+          title="HTML"
+        ></i>
+        <i
+          className="fa-brands fa-css3-alt skill-icon"
+          onClick={(e) => onClickIcon(e, "css")}
+          title="CSS"
+        ></i>
+        <i
+          className="fa-brands fa-python skill-icon"
+          onClick={(e) => onClickIcon(e, "python")}
+          title="Python"
+        ></i>
+        <i
+          className="fa-brands fa-node-js skill-icon"
+          onClick={(e) => onClickIcon(e, "nodejs")}
+          title="Node JS"
+        ></i>
       </div>
     </div>
   );
